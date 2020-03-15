46a52f70d21f79ece833092477b5161853461958
taka.usu <liverpenguin1408@gmail.com>
2020-03-15 13:19:11 +0000

    add bootstrap design

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# テストデータ作成
+# create ⇒  DBへ保存
+Message.create(title: 'test title 1', content: 'test content 1')
+Message.create(title: 'test title 2', content: 'test content 2')
+Message.create(title: 'test title 3', content: 'test content 3')
+Message.create(title: 'test title 4', content: 'test content 4')
+Message.create(title: 'test title 5', content: 'test content 5')
+Message.create(title: 'test title 6', content: 'test content 6')
